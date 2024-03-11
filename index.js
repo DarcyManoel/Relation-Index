d4d94db0e846b3be6701199ba714346c3706b3c5
DarcyManoel <darcman00@gmail.com>
2024-03-11 21:20:48 +1030

    Index entire Manoel/Manolis line

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+//example=[
+//	[],
+//	[
+//		[],
+//		[]
+//	],
+//	[],
+//	[]
+//]
 darcy2000=[
 	[`Darcy`,`Hamilton`,`Manoel`],
 	[
@@ -13,8 +22,8 @@ brandi1973=[
 		[22,7,1973],
 		[]
 	],
-	[],
-	[`darcy2000`]
+	[`David James Hamilton`,`Gloria Parslow`],
+	[`darcy2000`,`Monty Hamilton Manoel`,`Holly Crystal Manoel`]
 ]
 richard1967=[
 	[`Richard`,`John`,`Manoel`],
@@ -22,8 +31,8 @@ richard1967=[
 		[25,6,1967],
 		[]
 	],
-	[`raymond1933`],
-	[`darcy2000`]
+	[`raymond1933`,`Gwendoline LePoidevin`],
+	[`darcy2000`,`Monty Hamilton Manoel`,`Holly Crystal Manoel`]
 ]
 raymond1933=[
 	[`Raymond`,`Walter`,`Manoel`],
@@ -32,7 +41,7 @@ raymond1933=[
 		[10,9,2016]
 	],
 	[`george1907`,`Gladys Eleanor Silverlock`],
-	[`richard1967`],
+	[`richard1967`,`Gary Raymond Manoel`,`Wayne Anthony Manoel`,`Geoffrey Peter Manoel`,`Neil David Manoel`,`Wendy Joy Manoel`],
 ]
 george1907=[
 	[`George`,`William`,`Manoel`],
@@ -40,6 +49,24 @@ george1907=[
 		[`?`,`?`,1907],
 		[17,11,1990]
 	],
-	[`George Henry Manoel`,`Laura Dixon`],
+	[`george1873`,`Laura Dixon`],
 	[`raymond1933`,`Peter Manoel`,`Barry Manoel`,`Margaret Manoel`]
+]
+george1873=[
+	[`George`,`Henry`,`Manoel`],
+	[
+		[13,4,1873],
+		[17,11,1945]
+	],
+	[`peter1840`,`Martha Mary Creed Rimes`],
+	[`george1907`]
+]
+peter1840=[
+	[`Peter`,`Fredrick`,`Manolis`],
+	[
+		[15,10,1840],
+		[26,7,1914]
+	],
+	[`Nicholas Manolis`],
+	[`george1873`]
 ]
