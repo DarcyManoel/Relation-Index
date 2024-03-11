7a5c5d30d37961c983066dab7633234befc33a32
DarcyManoel <darcman00@gmail.com>
2024-03-11 22:34:22 +1030

    Allow for place of birth, marriage, death

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //example=[
 //	[],
 //	[
-//		[],
-//		[],
-//		[]
+//		[[],``],
+//		[[],``],
+//		[[],``]
 //	],
 //	[],
 //	[]
@@ -11,9 +11,9 @@
 darcy2000=[
 	[`Darcy`,`Hamilton`,`Manoel`],
 	[
-		[9,5,2000],
-		[],
-		[]
+		[[9,5,2000],``],
+		[[],``],
+		[[],``]
 	],
 	[`richard1967`,`brandi1973`],
 	[]
@@ -21,9 +21,9 @@ darcy2000=[
 brandi1973=[
 	[`Brandi`,`Hamilton`],
 	[
-		[22,7,1973],
-		[],
-		[]
+		[[22,7,1973],``],
+		[[],``],
+		[[],``]
 	],
 	[`David James Hamilton`,`Gloria Parslow`],
 	[`darcy2000`,`Monty Hamilton Manoel`,`Holly Crystal Manoel`]
@@ -31,9 +31,9 @@ brandi1973=[
 richard1967=[
 	[`Richard`,`John`,`Manoel`],
 	[
-		[25,6,1967],
-		[],
-		[]
+		[[25,6,1967],``],
+		[[],``],
+		[[],``]
 	],
 	[`raymond1933`,`Gwendoline LePoidevin`],
 	[`darcy2000`,`Monty Hamilton Manoel`,`Holly Crystal Manoel`]
@@ -41,9 +41,9 @@ richard1967=[
 raymond1933=[
 	[`Raymond`,`Walter`,`Manoel`],
 	[
-		[22,6,1933],
-		[],
-		[10,9,2016]
+		[[22,6,1933],``],
+		[[],``],
+		[[10,9,2016],``]
 	],
 	[`george1907`,`Gladys Eleanor Silverlock`],
 	[`richard1967`,`Gary Raymond Manoel`,`Wayne Anthony Manoel`,`Geoffrey Peter Manoel`,`Neil David Manoel`,`Wendy Joy Manoel`],
@@ -51,9 +51,9 @@ raymond1933=[
 george1907=[
 	[`George`,`William`,`Manoel`],
 	[
-		[`?`,`?`,1907],
-		[],
-		[17,11,1990]
+		[[`?`,`?`,1907],`Broken Hill, New South Wales, Australia`],
+		[[],``],
+		[[17,11,1990],`Manse, Semaphore, South Australia`]
 	],
 	[`george1873`,`Laura Dixon`],
 	[`raymond1933`,`Peter Manoel`,`Barry Manoel`,`Margaret Manoel`]
@@ -61,9 +61,9 @@ george1907=[
 george1873=[
 	[`George`,`Henry`,`Manoel`],
 	[
-		[13,4,1873],
-		[],
-		[17,11,1945]
+		[[13,4,1873],`Portland Estate, Pt Adelaide, South Australia`],
+		[[`?`,`?`,`?`],`Broken Hill, New South Wales, Australia`],
+		[[17,11,1945],`South Australia`]
 	],
 	[`peter1840`,`martha1848`],
 	[`george1907`]
@@ -71,9 +71,9 @@ george1873=[
 martha1848=[
 	[`Martha`,`Mary`,`Creed`,`Rimes`],
 	[
-		[`?`,9,1848],
-		[],
-		[22,7,1934]
+		[[`?`,9,1848],``],
+		[[],``],
+		[[22,7,1934],``]
 	],
 	[`daniel1807`,`Mary Creed`],
 	[`george1873`]
@@ -81,9 +81,9 @@ martha1848=[
 peter1840=[
 	[`Peter`,`Fredrick`,`Manolis`],
 	[
-		[15,10,1840],
-		[25,12,1868],
-		[26,7,1914]
+		[[15,10,1840],`Mitilini, Island of Lesbos, Greek Isles`],
+		[[25,12,1868],`Brompton, South Australia (Bowden)`],
+		[[26,7,1914],`Philip St, Southwark, South Australia`]
 	],
 	[`Nicholas Manolis`],
 	[`george1873`]
@@ -91,9 +91,9 @@ peter1840=[
 daniel1807=[
 	[`Daniel`,`Rimes`],
 	[
-		[`?`,`?`,1807],
-		[`?`,7,1840],
-		[`?`,10,1891]
+		[[`?`,`?`,1807],`Pearl, Somerset, England`],
+		[[`?`,7,1840],`England`],
+		[[`?`,10,1891],`Martlock, Somerset, England`]
 	],
 	[],
 	[`martha1848`]
--- scriptsheet.js
+++ scriptsheet.js
@@ -2,69 +2,74 @@ var focus=`darcy2000`
 var yearDiff
 function renderCard(){
 	document.getElementById(`main`).innerHTML=`<div id="title">`+window[focus][0].join(` `)+`</div>`
-	document.getElementById(`main`).innerHTML+=`<div id="lifePoints" class="back20">b. `+window[focus][1][0].join(`-`)+`</div>`
+	document.getElementById(`main`).innerHTML+=`<div id="lifePoints" class="back20">b. `+window[focus][1][0][0].join(`-`)+`</div>`
+	if(window[focus][1][0][1].length){
+		document.getElementById(`lifePoints`).innerHTML+=`<br><span class="list">`+window[focus][1][0][1]+`</span>`
+	}
 	yearDiff=0
-	if(window[focus][1][1].length){
-		document.getElementById(`lifePoints`).innerHTML+=`<br>m. `+window[focus][1][1].join(`-`)
-		yearDiff=window[focus][1][1][2]-window[focus][1][0][2]
-		if(window[focus][1][1][1]>window[focus][1][0][1]){
+	if(window[focus][1][1][0].length){
+		document.getElementById(`lifePoints`).innerHTML+=`<br>m. `+window[focus][1][1][0].join(`-`)
+		yearDiff=window[focus][1][1][0][2]-window[focus][1][0][0][2]
+		if(window[focus][1][1][0][2]==`?`||window[focus][1][0][0][2]==`?`){
+		}else if(window[focus][1][1][0][1]>window[focus][1][0][0][1]){
 			printYearRange(1)
-		}
-		else if(window[focus][1][1][1]<window[focus][1][0][1]){
+		}else if(window[focus][1][1][0][1]<window[focus][1][0][0][1]){
 			printYearRange(-1)
-		}
-		else if(window[focus][1][1][1]==window[focus][1][0][1]){
-			if(window[focus][1][1][0]>=window[focus][1][0][0]){
+		}else if(window[focus][1][1][0][1]==window[focus][1][0][0][1]){
+			if(window[focus][1][1][0][0]>=window[focus][1][0][0][0]){
 				printYearRange(1)
-			}
-			else if(window[focus][1][1][0]<window[focus][1][0][0]){
+			}else if(window[focus][1][1][0][0]<window[focus][1][0][0][0]){
 				printYearRange(-1)
-			}else if(window[focus][1][1][0]==`?`||window[focus][1][0][0]==`?`){
+			}else if(window[focus][1][1][0][0]==`?`||window[focus][1][0][0][0]==`?`){
 				printYearRange(0)
 			}
-		}else if(window[focus][1][1][1]==`?`||window[focus][1][0][1]==`?`){
+		}else if(window[focus][1][1][0][1]==`?`||window[focus][1][0][0][1]==`?`){
 			printYearRange(0)
 		}
 	}
-	if(window[focus][1][2].length){
-		document.getElementById(`lifePoints`).innerHTML+=`<br>d. `+window[focus][1][2].join(`-`)
-		yearDiff=window[focus][1][2][2]-window[focus][1][0][2]
-		if(window[focus][1][2][1]>window[focus][1][0][1]){
+	if(window[focus][1][1][1].length){
+		document.getElementById(`lifePoints`).innerHTML+=`<br><span class="list">`+window[focus][1][1][1]+`</span>`
+	}
+	if(window[focus][1][2][0].length){
+		document.getElementById(`lifePoints`).innerHTML+=`<br>d. `+window[focus][1][2][0].join(`-`)
+		yearDiff=window[focus][1][2][0][2]-window[focus][1][0][0][2]
+		if(window[focus][1][2][0][2]==`?`||window[focus][1][0][0][2]==`?`){
+		}else if(window[focus][1][2][0][1]>window[focus][1][0][0][1]){
 			printYearRange(1)
-		}
-		else if(window[focus][1][2][1]<window[focus][1][0][1]){
+		}else if(window[focus][1][2][0][1]<window[focus][1][0][0][1]){
 			printYearRange(-1)
-		}
-		else if(window[focus][1][2][1]==window[focus][1][0][1]){
-			if(window[focus][1][2][0]>=window[focus][1][0][0]){
+		}else if(window[focus][1][2][0][1]==window[focus][1][0][0][1]){
+			if(window[focus][1][2][0][0]>=window[focus][1][0][0][0]){
 				printYearRange(1)
-			}
-			else if(window[focus][1][2][0]<window[focus][1][0][0]){
+			}else if(window[focus][1][2][0][0]<window[focus][1][0][0][0]){
 				printYearRange(-1)
-			}else if(window[focus][1][2][0]==`?`||window[focus][1][0][0]==`?`){
+			}else if(window[focus][1][2][0][0]==`?`||window[focus][1][0][0][0]==`?`){
 				printYearRange(0)
 			}
-		}else if(window[focus][1][2][1]==`?`||window[focus][1][0][1]==`?`){
+		}else if(window[focus][1][2][0][1]==`?`||window[focus][1][0][0][1]==`?`){
 			printYearRange(0)
 		}
+	}
+	if(window[focus][1][2][1].length){
+		document.getElementById(`lifePoints`).innerHTML+=`<br><span class="list">`+window[focus][1][2][1]+`</span>`
 	}
 	if(window[focus][2].length){
-		document.getElementById(`main`).innerHTML+=`<div id="parents"><span class="back20">Parents:</span><br></div>`
+		document.getElementById(`main`).innerHTML+=`<br><div id="parents"><span class="back20">Parents:</span><br></div>`
 		for(i1=0;i1<window[focus][2].length;i1++){
 			if(containsUppercase(window[focus][2][i1])){
-				document.getElementById(`parents`).innerHTML+=`<span class="blank">`+window[focus][2][i1]+`</span><br>`
+				document.getElementById(`parents`).innerHTML+=`<span class="list">`+window[focus][2][i1]+`</span><br>`
 			}else{
-				document.getElementById(`parents`).innerHTML+=`<span onClick=changeFocus("`+window[focus][2][i1]+`")>`+window[window[focus][2][i1]][0].join(` `)+`</span><br>`
+				document.getElementById(`parents`).innerHTML+=`<span class="list link" onClick=changeFocus("`+window[focus][2][i1]+`")>`+window[window[focus][2][i1]][0].join(` `)+`</span><br>`
 			}
 		}
 	}
 	if(window[focus][3].length){
-		document.getElementById(`main`).innerHTML+=`<div id="children"><span class="back20">Children:</span><br></div>`
+		document.getElementById(`main`).innerHTML+=`<br><div id="children"><span class="back20">Children:</span><br></div>`
 		for(i1=0;i1<window[focus][3].length;i1++){
 			if(containsUppercase(window[focus][3][i1])){
-				document.getElementById(`children`).innerHTML+=`<span class="blank">`+window[focus][3][i1]+`</span><br>`
+				document.getElementById(`children`).innerHTML+=`<span class="list">`+window[focus][3][i1]+`</span><br>`
 			}else{
-				document.getElementById(`children`).innerHTML+=`<span onClick=changeFocus("`+window[focus][3][i1]+`")>`+window[window[focus][3][i1]][0].join(` `)+`</span><br>`
+				document.getElementById(`children`).innerHTML+=`<span class="list link" onClick=changeFocus("`+window[focus][3][i1]+`")>`+window[window[focus][3][i1]][0].join(` `)+`</span><br>`
 			}
 		}
 	}
