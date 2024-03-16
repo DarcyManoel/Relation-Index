82a39a56e7160aefd4a52f8bb57bf84c47c182c8
DarcyManoel <darcman00@gmail.com>
2024-03-16 18:33:16 +1030

    Include genders in index

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 darcy2000={
 	name:`Darcy Hamilton Manoel`,
+	gender:`male`,
 	birth:[9,5,2000],
 	parents:[`richard1967`,`brandi1973`]}
 richard1967={
 	name:`Richard John Manoel`,
+	gender:`male`,
 	birth:[25,6,1967],
 	parents:[`raymond1933`,`gwen1935`],
 	children:[`monty1999`,`darcy2000`,`holly2008`]}
 raymond1933={
 	name:`Raymond Walter Manoel`,
+	gender:`male`,
 	birth:[22,6,1933],
 	death:[10,9,2016],
 	parents:[`gladys1906`,`george1907`],
 	children:[`gary1953`,`wayne1955`,`geoffrey1958`,`neil1960`,`wendy1962`,`richard1967`]}
 gladys1906={
 	name:`Gladys Eleanor Silverlock`,
+	gender:`female`,
 	birth:[23,4,1906],
 	birthPlace:`Semaphore, Adelaide, South Australia`,
 	death:[`?`,`?`,1993],
@@ -22,293 +26,357 @@ gladys1906={
 	children:[`raymond1933`,`peter1944`]}
 walter1853={
 	name:`Walter Silverlock`,
+	gender:`male`,
 	birth:[`?`,`?`,1853],
 	marriage:[`?`,`?`,1882],
 	death:[`?`,`?`,1921],
 	children:[`gladys1906`,`Francis Ewart Everett Silverlock`]}
 gwen1932={
 	name:`Gwendoline Olive Le Poidevin`,
+	gender:`female`,
 	birth:[7,7,1932],
 	death:[7,7,1932],
 	parents:[`richard1889`,`olive1891`]}
 monty1999={
 	name:`Monty Hamilton Manoel`,
+	gender:`male`,
 	birth:[2,5,1999],
 	parents:[`richard1967`,`brandi1973`]}
 holly2008={
 	name:`Holly Crystal Manoel`,
+	gender:`female`,
 	birth:[17,4,2008],
 	parents:[`richard1967`,`brandi1973`]}
 brandi1973={
 	name:`Brandi Hamilton`,
+	gender:`female`,
 	birth:[22,7,1973],
 	parents:[`david1952`,`gloria1954`],
 	children:[`monty1999`,`darcy2000`,`holly2008`]}
 mitchell1989={
 	name:`Mitchell Gavin Alexander`,
+	gender:`male`,
 	birth:[23,4,1989],
 	parents:[`gavin1968`,`bernadette1970`]}
 melissa1988={
 	name:`Melissa Joy Manoel`,
+	gender:`female`,
 	birth:[31,10,1988],
 	parents:[`wayne1955`,`carolyn1958`]}
 shane1984={
 	name:`Shane Neil Manoel`,
+	gender:`male`,
 	birth:[30,3,1984],
 	parents:[`gayle1960`,`meil1960`]}
 sarah1983={
 	name:`Sarah Louise Manoel`,
+	gender:`female`,
 	birth:[27,10,1983],
 	parents:[`geoffrey1958`,`shelly1962`]}
 matthew1980={
 	name:`Matthew Geoffrey Manoel`,
+	gender:`male`,
 	birth:[21,8,1980],
 	parents:[`geoffrey1958`,`shelly1962`]}
 bernadette1970={
 	name:`Bernadette Mary Reedy`,
+	gender:`female`,
 	birth:[25,3,1970],
 	children:[`mitchell1989`]}
 gavin1968={
 	name:`Gavin Thomas Alexander`,
+	gender:`male`,
 	birth:[19,11,1968],
 	parents:[`thomas1939`,`ivy1940`],
 	children:[`mitchell1989`]}
 june1965={
 	name:`June Erica Alexander`,
+	gender:`female`,
 	birth:[18,10,1965],
 	parents:[`thomas1939`,`ivy1940`]}
 shelly1962={
 	name:`Shelly Louise Jones`,
+	gender:`female`,
 	birth:[10,10,1962],
 	children:[`matthew1980`,`sarah1983`]}
 wendy1962={
 	name:`Wendy Joy Manoel`,
+	gender:`female`,
 	birth:[3,2,1962],
 	parents:[`raymond1933`,`gwen1935`]}
 gayle1960={
 	name:`Gayle Ann Burke`,
+	gender:`female`,
 	birth:[10,5,1960],
 	children:[`shane1984`]}
 neil1960={
 	name:`Neil David Manoel`,
+	gender:`male`,
 	birth:[23,3,1960],
 	parents:[`raymond1933`,`gwen1935`],
 	children:[`shane1984`]}
 carolyn1958={
 	name:`Carolyn June Sanders`,
+	gender:`female`,
 	birth:[11,11,1958],
 	children:[`melissa1988`]}
 geoffrey1958={
 	name:`Geoffrey Peter Manoel`,
+	gender:`male`,
 	birth:[4,11,1958],
 	parents:[`raymond1933`,`gwen1935`],
 	children:[`matthew1980`,`sarah1983`]}
 carolyn1957={
 	name:`Carolyn Amy Liston`,
+	gender:`female`,
 	birth:[16,7,1957],
 	parents:[`ronald1913`,`amy1914`],
 	children:[`keith1980`,`jessica1983`,`eric1984`]}
 ivan1958={
 	name:`Ivan Keith Ashby`,
+	gender:`male`,
 	birth:[16,10,1958],
 	children:[`keith1980`,`jessica1983`,`eric1984`]}
 keith1980={
 	name:`Keith Wilfred Ashby`,
+	gender:`male`,
 	birth:[10,5,1980],
 	parents:[`carolyn1957`,`ivan1958`]}
 jessica1983={
 	name:`Jessica Amy Ashby`,
+	gender:`female`,
 	birth:[17,1,1983],
 	parents:[`carolyn1957`,`ivan1958`]}
 eric1984={
 	name:`Eric Ronald Ashby`,
+	gender:`male`,
 	birth:[9,3,1984],
 	parents:[`carolyn1957`,`ivan1958`]}
 wayne1955={
 	name:`Wayne Anthony Manoel`,
+	gender:`male`,
 	birth:[12,8,1955],
 	parents:[`raymond1933`,`gwen1935`],
 	children:[`melissa1988`]}
 gloria1954={
 	name:`Gloria Parslow`,
+	gender:`female`,
 	birth:[8,8,1954],
 	parents:[`marjorie1932`,`Ronald Stanley Parslow`],
 	children:[`brandi1973`]}
 gary1953={
 	name:`Gary Raymond Manoel`,
+	gender:`male`,
 	birth:[25,9,1953],
 	death:[8,12,2022],
 	parents:[`raymond1933`,`gwen1935`]}
 david1952={
 	name:`David James Hamilton`,
+	gender:`male`,
 	birth:[17,1,1952],
 	death:[5,8,2016],
 	parents:[`James Frederick Godfrey Hamilton`,`Gwendoline Joyce Woodcock`],
 	children:[`brandi1973`,`Danielle Ann`]}
 colin1951={
 	name:`Colin William Liston`,
+	gender:`male`,
 	birth:[25,9,1951],
 	parents:[`ronald1913`,`amy1914`],
 	children:[`rebecca1979`,`david1981`]}
 mary1955={
 	name:`Mary Frances Roper`,
+	gender:`female`,
 	birth:[10,3,1955],
 	children:[`rebecca1979`,`david1981`]}
 rebecca1979={
 	name:`Rebecca Claire Liston`,
+	gender:`female`,
 	birth:[9,4,1979],
 	parents:[`colin1951`,`mary1955`]}
 david1981={
 	name:`David Ronald Noel Liston`,
+	gender:`male`,
 	birth:[8,5,1981],
 	parents:[`colin1951`,`mary1955`]}
 peter1944={
 	name:`Peter Manoel`,
+	gender:`male`,
 	birth:[3,2,1944],
 	parents:[`gladys1906`,`george1907`]}
 gary1941={
 	name:`Gary John Ling`,
+	gender:`male`,
 	birth:[31,10,1941],
 	children:[`leona1962`,`karen1963`,`tanya1969`]}
 rhonda1941={
 	name:`Rhonda Aileen Liston`,
+	gender:`female`,
 	birth:[18,12,1941],
 	parents:[`ronald1913`,`amy1914`],
 	children:[`leona1962`,`karen1963`,`tanya1969`]}
 leona1962={
 	name:`Leona Jean Goldsworthy`,
+	gender:`female`,
 	birth:[6,8,1962],
 	parents:[`gary1941`,`rhonda1941`]}
 karen1963={
 	name:`Karen Marie Ling`,
+	gender:`female`,
 	birth:[25,10,1963],
 	parents:[`gary1941`,`rhonda1941`],
 	children:[`kyle1987`]}
 shane1961={
 	name:`Shane Malcolm Christopher`,
+	gender:`male`,
 	birth:[18,7,1961],
 	children:[`kyle1987`]}
 kyle1987={
 	name:`Kyle Harold Liston Christopher`,
+	gender:`male`,
 	birth:[17,10,1987],
 	parents:[`shane1961`,`karen1963`]}
 tanya1969={
 	name:`Tanya Marie Ling`,
+	gender:`female`,
 	birth:[23,3,1969],
 	parents:[`gary1941`,`rhonda1941`],
 	children:[`ryan1989`]}
 warren1963={
 	name:`Warren Grant Hatell`,
+	gender:`male`,
 	birth:[18,10,1963],
 	children:[`ryan1989`]}
 ryan1989={
 	name:`Ryan Corey Hartell`,
+	gender:`male`,
 	birth:[3,5,1989],
 	parents:[`warren1963`,`tanya1969`]}
 ivy1940={
 	name:`Ivy Lorraine Liston`,
+	gender:`female`,
 	birth:[1,6,1940],
 	parents:[`ronald1913`,`amy1914`],
 	children:[`june1965`,`gavin1968`]}
 thomas1939={
 	name:`Thomas Mitchell Alexander`,
+	gender:`male`,
 	birth:[1,10,1939],
 	death:[26,6,1986],
 	children:[`june1965`,`gavin1968`]}
 peter1938={
 	name:`Peter Le Poidevin`,
+	gender:`male`,
 	birth:[19,11,1938],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Luke Richard Le Poidevin`,`Sean Peter Le Poidevin`]}
 gwen1935={
 	name:`Gwendoline Le Poidevin`,
+	gender:`female`,
 	birth:[4,1,1935],
 	death:[30,4,2016],
 	parents:[`richard1889`,`olive1891`],
 	children:[`gary1953`,`wayne1955`,`geoffrey1958`,`neil1960`,`wendy1962`,`richard1967`]}
 marjorie1932={
 	name:`Marjorie Valda Williams`,
+	gender:`female`,
 	birth:[14,11,1932],
 	death:[25,2,2024],
 	parents:[`Hamilton Williams`,`Marjorie Schauer`],
 	children:[`gloria1954`]}
 joy1929={
 	name:`Joy Lorraine Le Poidevin`,
+	gender:`female`,
 	birth:[12,7,1929],
 	death:[2,6,1986],
 	parents:[`richard1889`,`olive1891`]}
 murray1927={
 	name:`Murray Russel Le Poidevin`,
+	gender:`male`,
 	birth:[16,11,1927],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Graham Richard Le Poidevin`,`Denise Anne Le Poidevin`,`Steven Murray Le Poidevin`]}
 joan1926={
 	name:`Joan Torquay Le Poidevin`,
+	gender:`female`,
 	birth:[24,7,1926],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Lynette Joan Bishop`,`Jennifer Kay Bishop`,`Barry Thomas Bishop`,`Meredith Anne Bishop`]}
 lillian1924={
 	name:`Lillian France Le Poidevin`,
+	gender:`female`,
 	birth:[25,9,1924],
 	parents:[`richard1889`,`olive1891`],
 	children:[`David Richard Bruce`,`Peter Dean Bruce`,`Ian Scott Bruce`]}
 gordon1924={
 	name:`Gordan Phillip Le Poidevin`,
+	gender:`male`,
 	birth:[25,9,1924],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Margaret Cloris Le Poidevin`,`Janet Key Le Poidevin`,`Helen Jean Le Poidevin`]}
 bennett1921={
 	name:`Bennett Teare Le Poidevin`,
+	gender:`male`,
 	birth:[19,6,1921],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Gaylene Ruth Le Poidevin`,`Jilliann Wynette Le Poidevin`,`Wayne Philip Le Poidevin`]}
 daisy1919={
 	name:`Daisy Millicent Le Poidevin`,
+	gender:`female`,
 	birth:[23,4,1919],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Barry Malcolm Dungey`]}
 edgar1917={
 	name:`Edgar George Le Poidevin`,
+	gender:`male`,
 	birth:[6,4,1917],
 	parents:[`richard1889`,`olive1891`],
 	children:[`Julie Marlene Le Poidevin`,`Mande Olive Le Poidevin`]}
 ivy1915={
 	name:`Ivy Esther Le Poidevin`,
+	gender:`female`,
 	birth:[21,10,1915],
 	parents:[`richard1889`,`olive1891`],
 	children:[`gerald1941`]}
 alexander1915={
 	name:`Alexander Collins`,
+	gender:`male`,
 	birth:[28,3,1915],
 	children:[`gerald1941`]}
 gerald1941={
 	name:`Gerald Alexander Collins`,
+	gender:`male`,
 	birth:[6,3,1941],
 	parents:[`alexander1915`,`ivy1915`],
 	children:[`stewart1966`,`catherine1967`]}
 jannette1941={
 	name:`Jannette Patricia Shave`,
+	gender:`female`,
 	birth:[19,5,1941],
 	children:[`stewart1966`,`catherine1967`]}
 stewart1966={
 	name:`Stewart Alexander Collins`,
+	gender:`male`,
 	birth:[18,1,1966],
 	parents:[`gerald1941`,`jannette1941`]}
 catherine1967={
 	name:`Catherine Anne Collins`,
+	gender:`female`,
 	birth:[27,8,1967],
 	parents:[`gerald1941`,`jannette1941`]}
 amy1914={
 	name:`Amy Patience Le Poidevin`,
+	gender:`female`,
 	birth:[8,3,1914],
 	parents:[`richard1889`,`olive1891`],
 	children:[`ivy1940`,`rhonda1941`,`colin1951`,`carolyn1957`]}
 ronald1913={
 	name:`Ronald Anstice Liston`,
+	gender:`male`,
 	birth:[3,8,1913],
 	children:[`ivy1940`,`rhonda1941`,`colin1951`,`carolyn1957`]}
 george1907={
 	name:`George William Manoel`,
+	gender:`male`,
 	birth:[`?`,`?`,1907],
 	birthPlace:`Broken Hill, New South Wales, Australia`,
 	death:[17,11,1990],
@@ -317,17 +385,20 @@ george1907={
 	children:[`raymond1933`,`peter1944`,`Barry Manoel`,`Margaret Manoel`]}
 sarah1861={
 	name:`Sarah Ann Everett`,
+	gender:`female`,
 	birth:[`?`,`?`,1861],
 	marriage:[`?`,`?`,1882],
 	death:[`?`,`?`,`?`],
 	children:[`gladys1906`,`Francis Ewart Everett Silverlock`]}
 olive1891={
 	name:`Olive Annie White`,
+	gender:`female`,
 	birth:[24,9,1891],
 	death:[14,2,1977],
 	children:[`amy1914`,`ivy1915`,`edgar1917`,`daisy1919`,`bennett1921`,`gordon1924`,`lillian1924`,`joan1926`,`murray1927`,`joy1929`,`gwen1932`,`gwen1935`,`peter1938`]}
 richard1889={
 	name:`Richard Gordan Le Poidevin`,
+	gender:`male`,
 	birth:[11,5,1889],
 	birthPlace:`Morphett Vale, South Australia`,
 	death:[5,5,1977],
@@ -336,6 +407,7 @@ richard1889={
 	children:[`amy1914`,`ivy1915`,`edgar1917`,`daisy1919`,`bennett1921`,`gordon1924`,`lillian1924`,`joan1926`,`murray1927`,`joy1929`,`gwen1932`,`gwen1935`,`peter1938`]}
 laura1877={
 	name:`Laura Dixon`,
+	gender:`female`,
 	birth:[14,4,1877],
 	birthPlace:`Moonta Mines, South Australia`,
 	death:[5,7,1961],
@@ -344,6 +416,7 @@ laura1877={
 	children:[`george1907`]}
 george1873={
 	name:`George Henry Manoel`,
+	gender:`male`,
 	birth:[13,4,1873],
 	birthPlace:`Portland Estate, Pt Adelaide, South Australia`,
 	marriage:[`?`,`?`,`?`],
@@ -354,12 +427,14 @@ george1873={
 	children:[`george1907`]}
 martha1848={
 	name:`Martha Mary Creed Rimes`,
+	gender:`female`,
 	birth:[`?`,9,1848],
 	death:[22,7,1934],
 	parents:[`daniel1807`,`mary1810`],
 	children:[`george1873`]}
 david1840={
 	name:`David William Dixon`,
+	gender:`male`,
 	birth:[15,3,1840],
 	birthPlace:`North Adelaide, South Australia`,
 	marriage:[24,7,1867],
@@ -370,6 +445,7 @@ david1840={
 	children:[`laura1877`]}
 peter1840={
 	name:`Peter Fredrick Manolis`,
+	gender:`male`,
 	birth:[15,10,1840],
 	birthPlace:`Mitilini, Island of Lesbos, Greek Isles`,
 	marriage:[25,12,1868],
@@ -381,6 +457,7 @@ peter1840={
 	children:[`george1873`]}
 daniel1826={
 	name:`Daniel Le Poidevin`,
+	gender:`male`,
 	birth:[19,5,1826],
 	birthPlace:`St Pierre du Bois, Guernsey, Channel Islands`,
 	marriage:[21,10,1852],
@@ -391,6 +468,7 @@ daniel1826={
 	children:[`richard1856`,`Thomas`,`Louisa`,`Martha`,`Alice`,`Alfred`,`Walter`,`Ada`,`Alfred`,`Amy`,`Laura Jones`,`Lionel`,`Howard`,`Ross`]}
 harriet1835={
 	name:`Harriet Palmer Hutchinson`,
+	gender:`female`,
 	birth:[4,10,1835],
 	birthPlace:`Ardwick, Manchester, England`,
 	marriage:[21,10,1852],
@@ -400,6 +478,7 @@ harriet1835={
 	children:[`richard1856`,`Thomas`,`Louisa`,`Martha`,`Alice`,`Alfred`,`Walter`,`Ada`,`Alfred`,`Amy`,`Laura Jones`,`Lionel`,`Howard`,`Ross`]}
 william1813={
 	name:`William Hutchinson`,
+	gender:`male`,
 	birth:[`?`,`?`,1813],
 	place:[18,2,1833],
 	death:[8,2,1884],
@@ -407,6 +486,7 @@ william1813={
 	children:[`eleanora1833`,`harriet1835`,`william1838`,`elizabeth1840`,`mary1843`,`john1845`,`eleanor1847`,`thomas1850`,`frederick1852`]}
 harriet1811={
 	name:`Harriet Smith Palmer`,
+	gender:`female`,
 	birth:[`?`,`?`,1811],
 	marriage:[18,2,1833],
 	death:[`?`,`?`,1880],
@@ -414,53 +494,63 @@ harriet1811={
 	children:[`eleanora1833`,`harriet1835`,`william1838`,`elizabeth1840`,`mary1843`,`john1845`,`eleanor1847`,`thomas1850`,`frederick1852`]}
 eleanora1833={
 	name:`Eleanora Christiana Whitfield Hutchinson`,
+	gender:`female`,
 	birth:[25,12,1833],
 	death:[`?`,`?`,1839],
 	parents:[`harriet1811`,`william1813`]}
 william1838={
 	name:`William Palmer Hutchinson`,
+	gender:`male`,
 	birth:[13,4,1838],
 	death:[2,8,1925],
 	parents:[`harriet1811`,`william1813`],
 	children:[`Joseph`,`Rhoda`,`Emily`,`Albert`,`Edwin`,`Harry`,`Frederick`,`Elanore`,`Annie`,`Bertha`,`Jessie`,`Herbert`]}
 mary1841={
 	name:`Mary Ann Middleton`,
+	gender:`female`,
 	birth:[26,9,1841],
 	death:[30,7,1922],
 	parents:[`harriet1811`,`william1813`],
 	children:[`Joseph`,`Rhoda`,`Emily`,`Albert`,`Edwin`,`Harry`,`Frederick`,`Elanore`,`Annie`,`Bertha`,`Jessie`,`Herbert`]}
 elizabeth1840={
 	name:`Elizabeth Palmer`,
+	gender:`female`,
 	birth:[27,8,1840],
 	death:[`?`,`?`,`?`],
 	parents:[`harriet1811`,`william1813`]}
 mary1843={
 	name:`Mary Palmer`,
+	gender:`female`,
 	birth:[15,2,1843],
 	death:[`?`,`?`,`?`],
 	parents:[`harriet1811`,`william1813`]}
 john1845={
 	name:`John Palmer`,
+	gender:`male`,
 	birth:[12,6,1845],
 	birthPlace:`Hackham`,
 	parents:[`harriet1811`,`william1813`]}
 eleanor1847={
 	name:`Eleanor`,
+	gender:`female`,
 	birth:[14,7,1847],
 	birthPlace:`Hackham`,
 	parents:[`harriet1811`,`william1813`]}
 thomas1850={
 	name:`Thomas Palmer`,
+	gender:`male`,
 	birth:[27,1,1850],
 	birthPlace:`Hackham`,
 	parents:[`harriet1811`,`william1813`]}
 frederick1852={
 	name:`Frederick Williams`,
+	gender:`male`,
 	birth:[`?`,`?`,1852],
 	birthPlace:`Hackham`,
 	parents:[`harriet1811`,`william1813`]}
 john1783={
 	name:`John Palmer`,
+	gender:`male`,
 	birth:[`?`,`?`,1783],
 	birthPlace:`Bishop Middleham, Durham`,
 	death:[`?`,`?`,1846],
@@ -468,6 +558,7 @@ john1783={
 	children:[`harriet1811`]}
 richard1856={
 	name:`Richard Morgan Le Poidevin`,
+	gender:`male`,
 	birth:[5,7,1856],
 	birthPlace:`Noarlunga, South Australia`,
 	death:[27,6,1913],
@@ -476,12 +567,14 @@ richard1856={
 	children:[`richard1889`]}
 joseph1825={
 	name:`Joseph Dixon`,
+	gender:`male`,
 	birth:[22,6,1825],
 	marriage:[6,12,1846],
 	death:[31,3,1890],
 	children:[`david1840`]}
 mary1810={
 	name:`Mary Creed`,
+	gender:`female`,
 	birth:[11,2,1810],
 	birthPlace:`West Pennard, Somerset, England`,
 	death:[`?`,6,1885],
@@ -489,12 +582,14 @@ mary1810={
 	children:[`martha1848`]}
 nicholas1810={
 	name:`Nicholas Manolis`,
+	gender:`male`,
 	birth:[`?`,`?`,1810],
 	birthPlace:`Mitilini, Island of Lesbos, Greek Isles`,
 	death:[`?`,`?`,`?`],
 	children:[`peter1840`]}
 daniel1807={
 	name:`Daniel Rimes`,
+	gender:`male`,
 	birth:[`?`,`?`,1807],
 	birthPlace:`Pearl, Somerset, England`,
 	marriage:[`?`,7,1840],
@@ -504,11 +599,13 @@ daniel1807={
 	children:[`martha1848`]}
 thomas1789={
 	name:`Thomas Le Poidevin`,
+	gender:`male`,
 	birth:[`?`,`?`,1789],
 	death:[`?`,`?`,1872],
 	children:[`daniel1826`]}
 martha1788={
 	name:`Martha Marquand`,
+	gender:`female`,
 	birth:[`?`,`?`,1788],
 	death:[`?`,`?`,`?`],
 	children:[`daniel1826`]}
